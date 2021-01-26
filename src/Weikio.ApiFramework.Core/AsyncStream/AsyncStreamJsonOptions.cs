110d6034c0f58eb3bdaba5407b484a3eb5cc4cc8
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-01-26 13:17:35 +0200

    Updated the default buffer size to 128

--- src/Weikio.ApiFramework.Core/AsyncStream/AsyncStreamJsonOptions.cs
+++ src/Weikio.ApiFramework.Core/AsyncStream/AsyncStreamJsonOptions.cs
@@ -4,8 +4,15 @@ namespace Weikio.ApiFramework.Core.AsyncStream
 {
     public class AsyncStreamJsonOptions
     {
+        /// <summary>
+        /// Gets or sets if Api Framework should try to create Streaming APIs automatically from IAsyncEnumerable-methods
+        /// </summary>
         public bool IsEnabled { get; set; } = true;
-        public int BufferSizeThreshold { get; set; } = 1024;
+        
+        /// <summary>
+        /// Gets or sets the default buffer size threshold used by Streaming API. Defaults to 128KB
+        /// </summary>
+        public int BufferSizeThreshold { get; set; } = 128 * 1000;
     }
 
 }
