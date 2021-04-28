4e29532f53e46020a1184bd954cf8961324c8f90
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-04-28 12:43:41 +0300

    Added functionality for configuring AutoTidyUrlMode on API level (#28)

---
+++ tests/Apis/HelloWorld/AnotherHelloWorldApi.cs
@@ -0,0 +1,10 @@
+namespace HelloWorld
+{
+    public class AnotherHelloWorldApi
+    {
+        public string SayHello()
+        {
+            return "Another Hello Api Framework!";
+        }
+    }
+}
